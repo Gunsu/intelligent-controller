1fd67e414a5ae6e8fe337c59f9b51c67c98a11ad
sergey.mogilchenko <sergey.mogilchenko@d313174e-fb83-11dd-802b-177316e4ac06>
2009-02-26 06:34:56 +0000

    [+] Добавлен mock-объект для теста Region-Manager-а.

---
+++ IC.Tests/WixProject.Test/Mock/MockRegion.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Practices.Composite.Regions;
+
+namespace IC.UI.WixProject.Mock
+{
+	public class MockRegion : IRegion
+	{
+		public List<object> AddedViews = new List<object>();
+
+		public IRegionManager Add(object view)
+		{
+			AddedViews.Add(view);
+			return null;
+		}
+
+		public void Remove(object view)
+		{
+			AddedViews.Remove(view);
+		}
+
+		public IViewsCollection Views
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public void Activate(object view)
+		{
+			SelectedItem = view;
+		}
+
+		public void Deactivate(object view)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IRegionManager Add(object view, string viewName)
+		{
+			Add(view);
+			return null;
+		}
+
+		public object GetView(string viewName)
+		{
+			return null;
+		}
+
+		public IRegionManager Add(object view, string viewName, bool createRegionManagerScope)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IRegionManager RegionManager
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		public object SelectedItem { get; set; }
+
+		public IViewsCollection ActiveViews
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+}
