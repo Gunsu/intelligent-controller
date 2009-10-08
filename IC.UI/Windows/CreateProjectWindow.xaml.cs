fa93cf440e1085cd5eeb80c756c24f6c0885c6f5
sergey.mogilchenko <sergey.mogilchenko@d313174e-fb83-11dd-802b-177316e4ac06>
2009-10-08 11:22:55 +0000

    [+] Добавлено добавление проектов на уровне UI

--- IC.UI/Windows/CreateProjectWindow.xaml.cs
+++ IC.UI/Windows/CreateProjectWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
-
+﻿using System.IO;
+using System.Windows;
+using IC.CoreInterfaces.Objects;
+using IC.CoreInterfaces.Processes;
+using IC.UI.Infrastructure.Events;
 using IC.UI.Infrastructure.Interfaces.Windows;
 using Microsoft.Practices.Composite.Events;
+using Microsoft.Win32;
 
 namespace IC.UI.Windows
 {
@@ -10,11 +14,45 @@ namespace IC.UI.Windows
 	/// </summary>
 	public partial class CreateProjectWindow : Window, ICreateProjectWindow
 	{
-		private readonly IEventAggregator eventAggregator;
+		private readonly IEventAggregator _eventAggregator;
+		private readonly IProjectProcesses _projectProcesses;
 
-		public CreateProjectWindow(IEventAggregator eventAggregator)
+		public CreateProjectWindow(IEventAggregator eventAggregator, IProjectProcesses projectProcesses)
 		{
 			InitializeComponent();
+			_eventAggregator = eventAggregator;
+			_projectProcesses = projectProcesses;
+		}
+
+		private void ChoosePath_Click(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog fileDialog = new SaveFileDialog();
+			fileDialog.AddExtension = true;
+			fileDialog.DefaultExt = "prj";
+			var result = fileDialog.ShowDialog();
+			if (result == true)
+			{
+				ProjectPath.Text = fileDialog.FileName;
+			}
+		}
+
+		private void Create_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(ProjectName.Text))
+			{
+				MessageBox.Show("Необходимо указать название проекта");
+				return;
+			}
+
+			if (string.IsNullOrEmpty(ProjectPath.Text))
+			{
+				MessageBox.Show("Необходимо указать путь к файлу проекта");
+				return;
+			}
+
+			IProject project = _projectProcesses.CreateProject(ProjectName.Text, ProjectPath.Text);
+			_eventAggregator.GetEvent<ProjectCreatedEvent>().Publish(project);
+			this.Close();
 		}
 	}
 }
