9045ca8f4bd0f717b82122b5304622024deea0fd
sergey.mogilchenko <sergey.mogilchenko@d313174e-fb83-11dd-802b-177316e4ac06>
2010-01-03 18:01:41 +0000

    [*] Подготовка к показу преподавателю

--- IC.Core/Entities/Compilation/Project.cs
+++ IC.Core/Entities/Compilation/Project.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using ValidationAspects;
 
 namespace IC.Core.Entities
 {
@@ -18,29 +14,13 @@ namespace IC.Core.Entities
 		/// </summary>
 		public List<Schema> Schemas { get; set; }
 
-		/// <summary>
-		/// Определяет, сохранён ли проект.
-		/// </summary>
-		[XmlIgnore]
-		public bool IsSaved { get; set; }
-
-		/// <summary>
-		/// Путь на жёстком диске, где лежит файл.
-		/// </summary>
-		public string FilePath { get; set; }
-
-		public string Name { get; set; }
-
-		[XmlIgnore]
 		public ROMData ROMData { get; private set; }
 
-		[XmlIgnore]
 		internal MemoryPool MemoryPool { get; private set; }
 
 		public Project()
 		{
 			Schemas = new List<Schema>();
-			IsSaved = false;
 			this.ROMData = new ROMData(Constants.ROM_DATA_SIZE);
 			this.MemoryPool = new MemoryPool(Constants.MEMORY_POOL_SIZE);
 		}
--- IC.Core/UISchemaParser.cs
+++ IC.Core/UISchemaParser.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using IC.Core.Entities.UI;
+using System.Linq;
+using System.Xml.Linq;
+using IC.Core.Abstract;
+using IC.Core.Entities;
 
 namespace IC.Core
 {
 	public class UISchemaParser
 	{
-		public Entities.Project Parse(Project uiProject)
-		{
-			var compilationProject = new Project();
+		private readonly IQueryable<BlockType> _blockTypes;
 
+		public UISchemaParser(IBlockTypesRepository blockTypesRepository)
+		{
+			_blockTypes = blockTypesRepository.LoadBlockTypesFromFile().AsQueryable();
+		}
+
+		public Entities.Project Parse(Entities.UI.Project uiProject)
+		{
+			var compilationProject = new Entities.Project();
+			foreach(var uiSchema in uiProject.Schemas)
+			{
+				var compilationSchema = new Entities.Schema() {Name = uiSchema.Name, Project = compilationProject};
+				compilationProject.Schemas.Add(compilationSchema);
+
+				var designerItems = uiSchema.CurrentUISchema.Element("DesignerItems");
+				foreach (XElement designerItem in designerItems.Nodes().OfType<XElement>())
+				{
+					var content = XElement.Parse(designerItem.Element("Content").Value);
+					int blockTypeID = GetBlockTypeID(content);
+					BlockType blockType = (from b in _blockTypes
+									       where b.ID == blockTypeID
+									       select b).First();
+					var block = new Block(blockType);
+					compilationSchema.Blocks.Add(block);
+				}
+
+				foreach (XElement element in uiSchema.CurrentUISchema.Elements())
+				{
+				}
+
+			}
 			throw new NotImplementedException();
+		}
+
+		private int GetBlockTypeID(XElement content)
+		{
+			foreach (XElement element in content.Nodes().OfType<XElement>())
+			{
+				if (element.Name.ToString() == "Tag" || element.Name.LocalName == "Image.Tag")
+				{
+					return int.Parse(element.Value);
+				}
+			}
+			throw new InvalidOperationException("Не найден необходимый элемент Tag с идентификатором блока.");
 		}
 	}
 }
